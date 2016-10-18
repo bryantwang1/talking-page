d68af7fe7863e84c3eaeaf2d0dd5a336820dfc3b
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-18 16:27:43 -0700

    Create basic tic-tac-toe game on image-insertion.html using jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -44,4 +44,148 @@ $(document).ready(function() {
     $(this).after("<p>Woof!</p>");
     $("button#meow").after("<p>Meow!</p>");
   });
+
+  $("button#top-left-x").click(function() {
+    $("button#top-left-o").after("<img src=\"img/xImage.png\">");
+
+    $("#top-left").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#top-left-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#top-left").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#top-middle-x").click(function() {
+    $("button#top-middle-o").after("<img src=\"img/xImage.png\">");
+
+    $("#top-middle").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#top-middle-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#top-middle").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#top-right-x").click(function() {
+    $("button#top-right-o").after("<img src=\"img/xImage.png\">");
+
+    $("#top-right").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#top-right-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#top-right").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#middle-left-x").click(function() {
+    $("button#middle-left-o").after("<img src=\"img/xImage.png\">");
+
+    $("#middle-left").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#middle-left-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#middle-left").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#middle-middle-x").click(function() {
+    $("button#middle-middle-o").after("<img src=\"img/xImage.png\">");
+
+    $("#middle-middle").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#middle-middle-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#middle-middle").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#middle-right-x").click(function() {
+    $("button#middle-right-o").after("<img src=\"img/xImage.png\">");
+
+    $("#middle-right").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#middle-right-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#middle-right").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#bottom-left-x").click(function() {
+    $("button#bottom-left-o").after("<img src=\"img/xImage.png\">");
+
+    $("#bottom-left").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#bottom-left-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#bottom-left").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#bottom-middle-x").click(function() {
+    $("button#bottom-middle-o").after("<img src=\"img/xImage.png\">");
+
+    $("#bottom-middle").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#bottom-middle-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#bottom-middle").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#bottom-right-x").click(function() {
+    $("button#bottom-right-o").after("<img src=\"img/xImage.png\">");
+
+    $("#bottom-right").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
+
+  $("button#bottom-right-o").click(function () {
+    $(this).after("<img src=\"img/operaO.png\">");
+
+    $("#bottom-right").children("img").first().click(function () {
+      $(this).remove();
+    });
+  });
 });
